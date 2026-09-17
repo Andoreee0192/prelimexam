cd80d550efae1501f95d79a6baaa6d6e9cd563c0
Edward Andrei Suva <edwardsuva0192@gmail.com>
2026-09-17 15:06:16 +0800

    Customize Next.js blog

--- pages/index.js
+++ pages/index.js
@@ -10,23 +10,38 @@ export default function Home() {
       </Head>
 
       <section className={utilStyles.headingMd}>
+        <h2>Why I Chose My IT Specialization</h2>
+
         <p>
-          Hi! I'm Edward Andrei Suva. I enjoy playing video games, listening to music, and spending time at cafes.
-          One of my favorite things to do is explore new cafes and try different kinds of coffee.
+          I chose Data Analytics as my IT specialization because I am interested
+          in how data can be used to solve real-world problems and help people
+          make better decisions. Many businesses and organizations collect large
+          amounts of data every day, but that data becomes useful only when it is
+          properly analyzed and understood.
         </p>
 
         <p>
-        I enjoy listening to different types of music and discovering new places to eat.
-         I also really enjoy coffee, especially when I get to experience it at a new cafe.
+          One of the industry trends that made me interested in Data Analytics is
+          the growing use of data-driven decision making. Companies use data to
+          understand customer behavior, improve services, identify problems,
+          predict trends, and make better business decisions.
         </p>
 
         <p>
-        In five years, I see myself working as a data analyst while continuing to explore new cafes, discover new places, and enjoy good coffee along the way.
+          I also enjoy working with technology, numbers, and problem solving.
+          Data Analytics allows me to use tools such as Python, databases,
+          spreadsheets, and data visualization to discover patterns and turn raw
+          data into useful information.
         </p>
 
         <p>
-          (This is a sample website - you’ll be building a site like this on our
-          Next.js tutorial.)
+          In the future, I see myself working as a data analyst and continuing to
+          improve my skills in data visualization, databases, and machine learning.
+        </p>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
