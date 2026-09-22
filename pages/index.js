ace361c6771d4a0c29f0b988c254fe7bf257f142
Edward Andrei Suva <edwardsuva0192@gmail.com>
2026-09-22 14:04:59 +0800

    Update prelim exam homepage

--- pages/index.js
+++ pages/index.js
@@ -10,38 +10,24 @@ export default function Home() {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <h2>Why I Chose My IT Specialization</h2>
+        <p>10 Things That Require Zero Talent</p>
+
+        <ul>
+          <li>Being On Time</li>
+          <li>Making An Effort</li>
+          <li>Being High Energy</li>
+          <li>Having A Positive Attitude</li>
+          <li>Being Passionate</li>
+          <li>Using Good Body Language</li>
+          <li>Being Coachable</li>
+          <li>Doing A Little Extra</li>
+          <li>Being Prepared</li>
+          <li>Having A Strong Work Ethic</li>
+        </ul>
 
         <p>
-          I chose Data Analytics as my IT specialization because I am interested
-          in how data can be used to solve real-world problems and help people
-          make better decisions. Many businesses and organizations collect large
-          amounts of data every day, but that data becomes useful only when it is
-          properly analyzed and understood.
-        </p>
-
-        <p>
-          One of the industry trends that made me interested in Data Analytics is
-          the growing use of data-driven decision making. Companies use data to
-          understand customer behavior, improve services, identify problems,
-          predict trends, and make better business decisions.
-        </p>
-
-        <p>
-          I also enjoy working with technology, numbers, and problem solving.
-          Data Analytics allows me to use tools such as Python, databases,
-          spreadsheets, and data visualization to discover patterns and turn raw
-          data into useful information.
-        </p>
-
-        <p>
-          In the future, I see myself working as a data analyst and continuing to
-          improve my skills in data visualization, databases, and machine learning.
-        </p>
-
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on our{' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
